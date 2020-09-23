d609e6fa5933afac04bda3591753da323db9fe9b
jiangzanchang <824173416@qq.com>
2020-09-23 16:35:11 +0800

    提交

---
+++ README-EN.md
@@ -0,0 +1,40 @@
+# Blog
+
+![](https://img.shields.io/badge/vue-2.5.2-brightgreen.svg)
+![](https://img.shields.io/badge/element--ui-2.3.5-brightgreen.svg)
+![](https://img.shields.io/badge/vant-1.1.2-brightgreen.svg)
+
+## [中文文档](README.md)
+
+## Demo
+
+[https://jzclem.github.io]
+
+
+## Plugin
+
+- Element (PC)
+- Vant (Mobile)
+
+## Use Setup
+
+Clone ```https://github.com/GitHub-Laziji/GitHub-Laziji.github.io ```
+
+
+## Develop
+
+#### Install Run Build
+
+    npm install
+
+    npm run dev
+
+    npm run build
+
+
+## License
+
+Code licensed under the [Mozilla](LICENSE).
+
+------
+
---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+    <div id="app">
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex'
+    export default {
+        name: 'App',
+        computed: {
+            ...mapGetters([
+                'githubUsername',
+                'htmlTitle'
+            ])
+        },
+        created() {
+            this.$store.dispatch("Init");
+            this.$store.dispatch("GetInfo");
+            this.$setTitle(this.$route.meta.title);
+            let windowSize = this.$util.getWindowSize();
+            let pathArr = this.$route.path.split("/");
+            if (pathArr[1] == "user" && windowSize.height > windowSize.width * 1.2) {
+                this.$router.push("/mobile/user/blog")
+            }
+            if (pathArr[1] == "mobile" && windowSize.height <= windowSize.width * 1.2) {
+                this.$router.push("/")
+            }
+        },
+        mounted() {
+          document.title = `被发现啦(*´∇｀*)`;
+          let visProp = this.getHiddenProp();
+          this.evtname = visProp.replace(/[H|h]idden/, '') + 'visibilitychange';
+          document.addEventListener(this.evtname, this.visibilityChange, false)
+        },
+        methods: {
+          getHiddenProp () {
+            var prefixes = ['webkit', 'moz', 'ms', 'o'];
+            // 如果本地支持“隐藏”，就返回它
+            if ('hidden' in document) {
+              return 'hidden'
+            }
+            // 否则，循环所有已知前缀，直到*找到一个
+            for (var i = 0; i < prefixes.length; i++) {
+              if ((prefixes[i] + 'Hidden') in document) {
+                return prefixes[i] + 'Hidden'
+              }
+            }
+            // 否则不支持
+            return null
+          },
+          getVisibilityState () {
+            var prefixes = ['webkit', 'moz', 'ms', 'o'];
+            if ('visibilityState' in document) return 'visibilityState';
+            for (var i = 0; i < prefixes.length; i++) {
+              if ((prefixes[i] + 'VisibilityState') in document) {
+                return prefixes[i] + 'VisibilityState'
+              }
+            }
+            // otherwise it's not supported 否则不支持
+            return null
+          },
+          visibilityChange () {
+            switch (document[this.getVisibilityState()]) {
+              case 'visible':
+                document.title = `被发现啦(*´∇｀*)`;
+                break;
+              case 'hidden':
+              default:
+                document.title = `藏好啦(つд⊂)`;
+                break;
+            }
+          }
+        }
+    }
+</script>
