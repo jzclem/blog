2bbc8a14610e7fe397baa45156e2141af80b401c
jiangzanchang <jiangzanchang@foresee.com.cn>
2021-12-08 11:28:50 +0800

    优化列表样式

--- src/mobile_views/blog/Main.vue
+++ src/mobile_views/blog/Main.vue
@@ -1,17 +1,20 @@
 <template>
   <div :style="'background: #f8f8f8;min-height: '+windowSize.height+'px'">
-    <van-nav-bar right-text="分享"
-                 style="position:fixed;top:0;z-index: 9999; box-shadow: 0px -3px 10px #888888;width: 100%;"
-                 title="博客列表"
-                 @click-right="$mobileShare()" />
+    <van-nav-bar right-text="分享" class="nav" title="博客列表" @click-right="$mobileShare()" />
     <div style="height: 60px;"></div>
-    <router-link v-for="(item,index) in blogs" :key="'p'+index" :to="`/mobile/user/blog/details/${item.id}`">
-      <van-panel :desc="'更新时间 '+item.update_time" :title="item.title" style="margin-bottom: 10px">
-        <div style="padding: 7px 15px 7px 15px;color: #303133;font-size: 0.9rem">{{
-            $util.cutStr(item.description, 50) }}
-        </div>
-      </van-panel>
-    </router-link>
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      :offset="20"
+      finished-text="没有更多了"
+      @load="list"
+    >
+      <van-cell class="cell" v-for="item in blogs" :key="item.id" @click="goDetails(item.id)">
+        <p class="title">{{ item.title }}</p>
+        <p class="time title">{{ '更新时间 ' + item.update_time }}</p>
+        <div class="content">{{ $util.cutStr(item.description, 48) }}</div>
+      </van-cell>
+    </van-list>
     <div style="height: 75px;"></div>
   </div>
 </template>
@@ -22,50 +25,62 @@
     data () {
       return {
         windowSize: this.$util.getWindowSize(),
+        loading: false,
+        finished: false,
         query: {
           page: 1,
-          pageSize: 20,
-          pageNumber: 1
+          pageSize: 6
         },
-        searchKey: '',
         blogs: []
       }
     },
     mounted () {
-
       this.list()
     },
     methods: {
-      test () {
-
-      },
       list () {
-        this.$toast.loading({
-          duration: 0,
-          forbidClick: true,
-          message: '加载中'
-        })
+        this.loading = true
         GistApi.list(this.query).then((response) => {
           let result = response.data.data
-          let pageNumber = this.$util.parseHeaders(response.headers)
-          if (pageNumber) {
-            this.query.pageNumber = pageNumber
-          }
-          if (result.length == 0) {
-            return
-          }
-          this.blogs = result
-        }).then(() => this.$toast.clear())
-      },
-      search () {
-        for (let i = 0; i < this.blogs.length; i++) {
-          this.blogs[i].hide = this.blogs[i].title.indexOf(this.searchKey) < 0
-        }
+          this.blogs.push(...result)
+          this.finished = result.length < ++this.query.page
+        }).then(() => this.loading = false)
       },
       goDetails (id) {
-        console.log(id)
-        this.$router.push('' + id)
+        this.$router.push(`/mobile/user/blog/details/${id}`)
       }
     }
   }
 </script>
+
+<style scoped>
+  .nav {
+    position:fixed;
+    top:0;
+    z-index: 9999;
+    box-shadow: 0 -3px 10px #888888;
+    width: 100%;
+  }
+  .title {
+    margin: 0;
+  }
+  .time {
+    font-size: 0.7rem;
+    margin-bottom: 7px;
+  }
+  .content {
+    padding: 7px 15px 7px 0;
+    color: #303133;
+    font-size: 0.9rem;
+    border-top: 1px solid #ebedf0;
+  }
+  .cell {
+    margin: 10px 0;
+  }
+  [class*=van-hairline]::after {
+    border: 0;
+  }
+  .van-list .cell:first-child {
+    margin: 0;
+  }
+</style>
